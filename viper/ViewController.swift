357cf7ba01e05831ec899300dbddc49277b7493b
vigotskij <boris.sortino@gmail.com>
2019-12-15 11:50:05 -0300

    creating project

---
+++ viper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  viper
+//
+//  Created by Boris Sortino on 15/12/2019.
+//  Copyright © 2019 Boris Sortino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
